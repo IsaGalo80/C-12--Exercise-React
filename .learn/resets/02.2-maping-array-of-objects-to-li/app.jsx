2c610675b96793f717ed02326b8e19295c97dfc9
IsaGalo80 <isagalo1980@outlook.com>
2022-06-22 10:35:31 +0000

    aún faltan hacer ejercicios

--- .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
+++ .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
@@ -8,11 +8,9 @@ const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
  * planets in html called planetsInHTML
  */
 
-var planList = planets.map(function (planeta,indice) {
-	return <li className= "list-group-item" key={indice} >{planeta}</li>;
-});
-console.log(planList);
+
+
 // 2) add the array planetsInHTML inside the innerHTML of this ul
-const content = <ul className="list-group m-5">{planList}</ul>;
+const content = (<ul className="list-group m-5"></ul>);
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
--- exercises/02.1-maping-array-objects-to-li/app.jsx
+++ exercises/02.1-maping-array-objects-to-li/app.jsx
@@ -7,16 +7,8 @@ const animals = [{ label: "Horse" }, { label: "Turtle" }, { label: "Elephant" },
  * change the content of the map function
  **/
 const animalsInHTML = animals.map((singleAnimal, i) => {
-	return <li>hello</li>;
+	return <li>{singleAnimal.label}</li>;
 });
 
 ReactDOM.render(<ul>{animalsInHTML}</ul>, document.querySelector("#myDiv"));
 
-// // Lo que tienes
-// var oficiales = [
-// 	{ id: 20, nombre: 'Capitán Piett' },
-// 	{ id: 24, nombre: 'General Veers' },
-// 	{ id: 56, nombre: 'Almirante Ozzel' },
-// 	{ id : 88, nombre: 'Comandante Jerjerrod' }
-//   ];
-// const animals = animals.map(animals => animals.length);
